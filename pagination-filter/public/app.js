95b553af4ed98757b4a3d317b3ef5fa2fbaacf97
Irina Capatina <irina.capatina@yahoo.com>
2016-11-18 10:28:26 +0200

    commit with pagination done

--- pagination-filter/public/app.js
+++ pagination-filter/public/app.js
@@ -2,53 +2,240 @@ angular.module('app', []);
 
 angular
 	.module('app')
-	.filter('StartFrom', [StartFrom]);
+	.controller('MainController', [MainController]);
 
-function StartFrom () {
-	return function (input, start) {
-		start = +start;
-		return input.slice(start);
-	};
-}
-
-angular
-	.module('app')
-	.controller('PaginationController', [PaginationController]);
-
-function PaginationController () {
+function MainController () {
 	var self = this;
 
-	self.currentPage = 0;
-	self.itemsPerPage = 3;
-	self.collection = [];
+	// self.searchFunction = function searchFunction (item) {
+	// 	// return item.price > 1010;
+	// };
 
-	self.mainPage = function mainPage () {
-		return self.currentPage == 0;
+	self.currentPage = 1;
+	self.itemsPerPage = 10;
+	
+	self.collection = [
+		{
+			name: 'Phone',
+			price: '500',
+			deliveryDate: new Date(),
+			description: 'description1'
+		},
+		{
+			name: 'TV',
+			price: '700',
+			deliveryDate: new Date(),
+			description: 'description2'
+		},
+		{
+			name: 'Watch',
+			price: '360',
+			deliveryDate: new Date(),
+			description: 'description3'
+		},
+		{
+			name: 'Videogame',
+			price: '800',
+			deliveryDate: new Date(),
+			description: 'description4'
+		},
+		{
+			name: 'Tablet',
+			price: '580',
+			deliveryDate: new Date(),
+			description: 'description5'
+		},
+		{
+			name: 'Phone',
+			price: '100',
+			deliveryDate: new Date(),
+			description: 'description1'
+		},
+		{
+			name: 'TV',
+			price: '300',
+			deliveryDate: new Date(),
+			description: 'description2'
+		},
+		{
+			name: 'TV',
+			price: '50',
+			deliveryDate: new Date(),
+			description: 'description3'
+		},
+		{
+			name: 'Phone',
+			price: '500',
+			deliveryDate: new Date(),
+			description: 'description1'
+		},
+		{
+			name: 'TV',
+			price: '700',
+			deliveryDate: new Date(),
+			description: 'description2'
+		},
+		{
+			name: 'Watch',
+			price: '360',
+			deliveryDate: new Date(),
+			description: 'description3'
+		},
+		{
+			name: 'Videogame',
+			price: '800',
+			deliveryDate: new Date(),
+			description: 'description4'
+		},
+		{
+			name: 'Tablet',
+			price: '580',
+			deliveryDate: new Date(),
+			description: 'description5'
+		},
+		{
+			name: 'Phone',
+			price: '100',
+			deliveryDate: new Date(),
+			description: 'description1'
+		},
+		{
+			name: 'TV',
+			price: '300',
+			deliveryDate: new Date(),
+			description: 'description2'
+		},
+		{
+			name: 'TV',
+			price: '50',
+			deliveryDate: new Date(),
+			description: 'description3'
+		},
+		{
+			name: 'Phone',
+			price: '500',
+			deliveryDate: new Date(),
+			description: 'description1'
+		},
+		{
+			name: 'TV',
+			price: '700',
+			deliveryDate: new Date(),
+			description: 'description2'
+		},
+		{
+			name: 'Watch',
+			price: '360',
+			deliveryDate: new Date(),
+			description: 'description3'
+		},
+		{
+			name: 'Videogame',
+			price: '800',
+			deliveryDate: new Date(),
+			description: 'description4'
+		},
+		{
+			name: 'Tablet',
+			price: '580',
+			deliveryDate: new Date(),
+			description: 'description5'
+		},
+		{
+			name: 'Phone',
+			price: '100',
+			deliveryDate: new Date(),
+			description: 'description1'
+		},
+		{
+			name: 'TV',
+			price: '300',
+			deliveryDate: new Date(),
+			description: 'description2'
+		},
+		{
+			name: 'TV',
+			price: '50',
+			deliveryDate: new Date(),
+			description: 'description3'
+		},{
+			name: 'Phone',
+			price: '500',
+			deliveryDate: new Date(),
+			description: 'description1'
+		},
+		{
+			name: 'TV',
+			price: '700',
+			deliveryDate: new Date(),
+			description: 'description2'
+		},
+		{
+			name: 'Watch',
+			price: '360',
+			deliveryDate: new Date(),
+			description: 'description3'
+		},
+		{
+			name: 'Videogame',
+			price: '800',
+			deliveryDate: new Date(),
+			description: 'description4'
+		},
+		{
+			name: 'Tablet',
+			price: '580',
+			deliveryDate: new Date(),
+			description: 'description5'
+		},
+		{
+			name: 'Phone',
+			price: '100',
+			deliveryDate: new Date(),
+			description: 'description1'
+		},
+		{
+			name: 'TV',
+			price: '300',
+			deliveryDate: new Date(),
+			description: 'description2'
+		},
+		{
+			name: 'TV',
+			price: '50',
+			deliveryDate: new Date(),
+			description: 'description3'
+		}
+	];
+
+	self.mainPage = function () {
+		return self.currentPage == 2;
 	};
 
-	self.lastPage = function lastPage () {
-		var lastPageNr = Math.ceil(self.collection.length / self.itemsNr - 1);
+	self.lastPage = function () {
+		var lastPageNr = Math.ceil(self.collection.length / self.itemsPerPage - 1);
 		return self.currentPage == lastPageNr;
 	};
 
-	self.nrOfPages = function nrOfPages () {
-		return Math.ceil(self.collection.length / self.itemsNr);
+	self.nrOfPages = function () {
+		return Math.ceil(self.collection.length / self.itemsPerPage);
 	};
 
-	self.startingItem = function startingItem () {
-		return self.currentPage * self.itemsNr;
+	self.startingItem = function () {
+		return self.currentPage * self.itemsPerPage;
 	};
 
-	self.pageBack = function pageBack () {
+	self.pageBack = function () {
 		self.currentPage = self.currentPage - 1;
 	};
 
-	self.pageForward = function pageForward () {
+	self.pageForward = function () {
 		self.currentPage = self.currentPage + 1;
 	};
 
 }
 
+	// search by price > 500
 angular
 	.module('app')
 	.filter('PriceFilter', ['$sce', PriceFilter]);
@@ -56,7 +243,7 @@ angular
 function PriceFilter ($sce) {
 	return function (value) {
 		if(value >= '500') {
-			return $sce.trustAsHtml('<strong>' + value + '</strong>');
+			return $sce.trustAsHtml('<strong><i>' + value + '</strong></i>');
 		} else {
 			return $sce.trustAsHtml(value);
 		}
@@ -65,106 +252,10 @@ function PriceFilter ($sce) {
 
 angular
 	.module('app')
-	.controller('MainController', [MainController]);
+	.filter('StartFrom', [StartFrom]);
 
-function MainController () {
-	var self = this;
-
-	self.searchFunction = function searchFunction (item) {
-		return item.price > 1010;
+function StartFrom () {
+	return function (input, start) {
+		return input.slice(start);
 	};
-
-	self.collection = [
-		{
-			name: 'Phone',
-			price: '500',
-			deliveryDate: new Date(),
-			description: 'description1'
-		},
-			{
-			name: 'TV',
-			price: '700',
-			deliveryDate: new Date(),
-			description: 'description2'
-		},
-			{
-			name: 'e-book',
-			price: '50',
-			deliveryDate: new Date(),
-			description: 'description3'
-		},
-			{
-			name: 'tegbhg',
-			price: '12',
-			deliveryDate: new Date(),
-			description: 'description4'
-		},
-			{
-			name: 'pppp',
-			price: '58',
-			deliveryDate: new Date(),
-			description: 'description5'
-		},
-			{
-			name: 'Phone',
-			price: '500',
-			deliveryDate: new Date(),
-			description: 'description1'
-		},
-			{
-			name: 'TV',
-			price: '700',
-			deliveryDate: new Date(),
-			description: 'description2'
-		},
-			{
-			name: 'e-book',
-			price: '50',
-			deliveryDate: new Date(),
-			description: 'description3'
-		},
-			{
-			name: 'tegbhg',
-			price: '12',
-			deliveryDate: new Date(),
-			description: 'description4'
-		},
-			{
-			name: 'pppp',
-			price: '58',
-			deliveryDate: new Date(),
-			description: 'description5'
-		},
-			{
-			name: 'Phone',
-			price: '500',
-			deliveryDate: new Date(),
-			description: 'description1'
-		},
-			{
-			name: 'TV',
-			price: '700',
-			deliveryDate: new Date(),
-			description: 'description2'
-		},
-			{
-			name: 'e-book',
-			price: '50',
-			deliveryDate: new Date(),
-			description: 'description3'
-		},
-			{
-			name: 'tegbhg',
-			price: '12',
-			deliveryDate: new Date(),
-			description: 'description4'
-		},
-			{
-			name: 'pppp',
-			price: '58',
-			deliveryDate: new Date(),
-			description: 'description5'
-		}
-	];
-
 }
